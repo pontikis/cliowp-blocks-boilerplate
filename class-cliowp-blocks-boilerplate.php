effaf03b1492f3ae83088765802bbdabe486a1ed
Christos Pontikis <pontikis@gmail.com>
2022-09-22 17:11:31 +0300

    main php class

--- class-cliowp-blocks-boilerplate.php
+++ class-cliowp-blocks-boilerplate.php
@@ -22,6 +22,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * ClioWP_Blocks_Boilerplate plugin main class
+ *
+ * @link https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/writing-your-first-block-type/
+ * @link https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/
  */
 class ClioWP_Blocks_Boilerplate {
 
@@ -34,17 +37,38 @@ class ClioWP_Blocks_Boilerplate {
 
 	/**
 	 * Register assets and block
+	 *
+	 * It is recommended to use block.json file after version 5.8
+	 * However if you don't use it, register_block would be like
+	 * the following register_block_without_block_json() function
 	 */
 	public function register_block() {
-		wp_register_script( 'blocks_boilerplate_js', plugin_dir_url( __FILE__ ) . 'build/index.js', array( 'wp-blocks', 'wp-i18n', 'wp-editor' ), '1.0.0', true );
-		wp_register_style( 'blocks_boilerplate_css', plugin_dir_url( __FILE__ ) . 'build/index.css', array(), '1.0.0' );
-
 		register_block_type(
-			'cliowp-blocks/boilerplate',
+			__DIR__,
 			array(
 				'render_callback' => array( $this, 'render_callback' ),
-				'editor_script'   => 'blocks_boilerplate_js',
-				'editor_style'    => 'blocks_boilerplate_css',
+			)
+		);
+	}
+
+	/**
+	 * Register assets and block (without using block.json file)
+	 * Unused fucntion - just for reference
+	 */
+	public function register_block_without_block_json() {
+		wp_register_script( 'editor_js', plugin_dir_url( __FILE__ ) . 'build/editor.js', array( 'wp-blocks', 'wp-i18n', 'wp-editor' ), '1.0.0', true );
+		wp_register_script( 'frontend_js', plugin_dir_url( __FILE__ ) . 'build/editor.js', array(), '1.0.0', true );
+		wp_register_style( 'editor_css', plugin_dir_url( __FILE__ ) . 'build/frontend.css', array(), '1.0.0' );
+		wp_register_style( 'frontend_css', plugin_dir_url( __FILE__ ) . 'build/frontend.css', array(), '1.0.0' );
+
+		register_block_type(
+			'cliowp-blocks/hello-gutenberg',
+			array(
+				'render_callback' => array( $this, 'render_callback' ),
+				'editor_script'   => 'editor_js',
+				'editor_style'    => 'editor_css',
+				'script'          => 'frontend_js',
+				'style'           => 'frontend_css',
 			)
 		);
 	}
@@ -55,7 +79,7 @@ class ClioWP_Blocks_Boilerplate {
 	 * @param array $attributes The attributes to pass to PHP.
 	 */
 	public function render_callback( $attributes ) {
-		return '<p>Hello Gutenberg!</p>';
+		return '<p class="paint-it-yellow">Hello Gutenberg!</p>';
 	}
 
 }
