48dc442017fab21c485256d0057ec90e1a615239
Christos Pontikis <pontikis@gmail.com>
2022-11-01 15:23:46 +0200

    build

--- build/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '235b98fec25cd5c74d89');
--- build/frontend.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'ef69de2264c68e03c6af');
